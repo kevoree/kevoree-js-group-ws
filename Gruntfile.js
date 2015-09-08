1ee692866240651ef4893f965391aedf3787beb6
Maxime Tricoire <max.tricoire@gmail.com>
2015-09-08 16:01:14 +0200

    improvement of the broadcast

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', 'build');
-    grunt.registerTask('build', ['kevoree_genmodel', 'browser']);
+    grunt.registerTask('build', 'Build Kevoree module', function () {
+      if (process.env.KEVOREE_RUNTIME !== 'dev') {
+        grunt.tasks(['kevoree_genmodel', 'browser']);
+      }
+    });
     grunt.registerTask('publish', ['kevoree_registry']);
     grunt.registerTask('kev', ['kevoree']);
     grunt.registerTask('browser', ['browserify', 'uglify']);
